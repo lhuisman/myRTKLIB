c66fbeba3a4d4a712e38733fb792567c23469329
Tim Everett <tim.everett3@gmail.com>
2019-08-14 09:20:23 -0600

    - Update project versions in build files for newer Embarcadero compiler - Minor plotting fix for previous change

--- app/rtkplot/rtkplot.cpp
+++ app/rtkplot/rtkplot.cpp
@@ -42,7 +42,6 @@
 
 
 
-USEFORM("vmapdlg.cpp", VecMapDialog);
 USEFORM("..\appcmn\tcpoptdlg.cpp", TcpOptDialog);
 USEFORM("..\appcmn\serioptdlg.cpp", SerialOptDialog);
 USEFORM("..\appcmn\refdlg.cpp", RefDialog);
@@ -57,15 +56,15 @@ USEFORM("..\appcmn\aboutdlg.cpp", AboutDialog);
 USEFORM("..\appcmn\console.cpp", Console);
 USEFORM("..\appcmn\ftpoptdlg.cpp", FtpOptDialog);
 USEFORM("..\appcmn\fileoptdlg.cpp", FileOptDialog);
+USEFORM("conndlg.cpp", ConnectDialog);
 USEFORM("pntdlg.cpp", PntDialog);
 USEFORM("plotopt.cpp", PlotOptDialog);
-USEFORM("plotmain.cpp", Plot);
+USEFORM("vmapdlg.cpp", VecMapDialog);
 USEFORM("skydlg.cpp", SkyImgDialog);
 USEFORM("satdlg.cpp", SatDialog);
-USEFORM("gmview.cpp", GoogleMapView);
-USEFORM("geview.cpp", GoogleEarthView);
-USEFORM("conndlg.cpp", ConnectDialog);
+USEFORM("plotmain.cpp", Plot);
 USEFORM("mapdlg.cpp", MapAreaDialog);
+USEFORM("gmview.cpp", GoogleMapView);
 //---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
@@ -74,30 +73,29 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		Application->Initialize();
 		Application->Title = "RTKPLOT";
 		Application->CreateForm(__classid(TPlot), &Plot);
-         Application->CreateForm(__classid(TPlotOptDialog), &PlotOptDialog);
-         Application->CreateForm(__classid(TSatDialog), &SatDialog);
-         Application->CreateForm(__classid(TRefDialog), &RefDialog);
-         Application->CreateForm(__classid(TAboutDialog), &AboutDialog);
-         Application->CreateForm(__classid(TSpanDialog), &SpanDialog);
-         Application->CreateForm(__classid(TTimeDialog), &TimeDialog);
-         Application->CreateForm(__classid(TConnectDialog), &ConnectDialog);
-         Application->CreateForm(__classid(TSerialOptDialog), &SerialOptDialog);
-         Application->CreateForm(__classid(TTcpOptDialog), &TcpOptDialog);
-         Application->CreateForm(__classid(TCmdOptDialog), &CmdOptDialog);
-         Application->CreateForm(__classid(TFileOptDialog), &FileOptDialog);
-         Application->CreateForm(__classid(TKeyDialog), &KeyDialog);
-         Application->CreateForm(__classid(TTextViewer), &TextViewer);
-         Application->CreateForm(__classid(TViewerOptDialog), &ViewerOptDialog);
-         Application->CreateForm(__classid(TPntDialog), &PntDialog);
-         Application->CreateForm(__classid(TConfDialog), &ConfDialog);
-         Application->CreateForm(__classid(TGoogleEarthView), &GoogleEarthView);
-         Application->CreateForm(__classid(TFtpOptDialog), &FtpOptDialog);
-         Application->CreateForm(__classid(TConsole), &Console);
-         Application->CreateForm(__classid(TGoogleMapView), &GoogleMapView);
-         Application->CreateForm(__classid(TMapAreaDialog), &MapAreaDialog);
-         Application->CreateForm(__classid(TSkyImgDialog), &SkyImgDialog);
-         Application->CreateForm(__classid(TVecMapDialog), &VecMapDialog);
-         Application->Run();
+		Application->CreateForm(__classid(TPlotOptDialog), &PlotOptDialog);
+		Application->CreateForm(__classid(TSatDialog), &SatDialog);
+		Application->CreateForm(__classid(TRefDialog), &RefDialog);
+		Application->CreateForm(__classid(TAboutDialog), &AboutDialog);
+		Application->CreateForm(__classid(TSpanDialog), &SpanDialog);
+		Application->CreateForm(__classid(TTimeDialog), &TimeDialog);
+		Application->CreateForm(__classid(TConnectDialog), &ConnectDialog);
+		Application->CreateForm(__classid(TSerialOptDialog), &SerialOptDialog);
+		Application->CreateForm(__classid(TTcpOptDialog), &TcpOptDialog);
+		Application->CreateForm(__classid(TCmdOptDialog), &CmdOptDialog);
+		Application->CreateForm(__classid(TFileOptDialog), &FileOptDialog);
+		Application->CreateForm(__classid(TKeyDialog), &KeyDialog);
+		Application->CreateForm(__classid(TTextViewer), &TextViewer);
+		Application->CreateForm(__classid(TViewerOptDialog), &ViewerOptDialog);
+		Application->CreateForm(__classid(TPntDialog), &PntDialog);
+		Application->CreateForm(__classid(TConfDialog), &ConfDialog);
+		Application->CreateForm(__classid(TFtpOptDialog), &FtpOptDialog);
+		Application->CreateForm(__classid(TConsole), &Console);
+		Application->CreateForm(__classid(TGoogleMapView), &GoogleMapView);
+		Application->CreateForm(__classid(TMapAreaDialog), &MapAreaDialog);
+		Application->CreateForm(__classid(TSkyImgDialog), &SkyImgDialog);
+		Application->CreateForm(__classid(TVecMapDialog), &VecMapDialog);
+		Application->Run();
 	}
 	catch (Exception &exception)
 	{
