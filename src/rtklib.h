b254e7af4564e15041e025fd8687a5f8fef9442b
Tim Everett <tim.everett3@gmail.com>
2019-11-16 12:16:28 -0700

    - Uodate version to b33b2

--- src/rtklib.h
+++ src/rtklib.h
@@ -58,7 +58,7 @@ extern "C" {
 
 #define VER_RTKLIB  "demo5"             /* library version */
 
-#define PATCH_LEVEL "b33b"               /* patch level */
+#define PATCH_LEVEL "b33b2"               /* patch level */
 
 #define COPYRIGHT_RTKLIB \
             "Copyright (C) 2007-2019 T.Takasu\nAll rights reserved."
