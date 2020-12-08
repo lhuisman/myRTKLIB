ac6fa168c34c788bb0ad72e24a9d196bbc7d272b
Tim Everett <tim.everett3@gmail.com>
2020-12-08 13:05:13 -0700

    - Reduced max Beidou PRN to 46 to reduce memory and CPU requirements based on the understanding that sats above this number are very sparse.

--- src/rtklib.h
+++ src/rtklib.h
@@ -176,7 +176,7 @@ extern "C" {
 #endif
 #ifdef ENACMP
 #define MINPRNCMP   1                   /* min satellite sat number of BeiDou */
-#define MAXPRNCMP   61                  /* max satellite sat number of BeiDou */
+#define MAXPRNCMP   46                  /* max satellite sat number of BeiDou */
 #define NSATCMP     (MAXPRNCMP-MINPRNCMP+1) /* number of BeiDou satellites */
 #define NSYSCMP     1
 #else
