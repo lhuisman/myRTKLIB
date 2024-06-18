ourairquality/RTKLIB

rtklib interface const char
For the rtklib interface functions, declare char pointer arguments as
'const char ...' when the function does not modify the data. Many were
already declared.

Some follow on patches are also included for some of the callees for
consistency, and some of the caller arguments needed type casts.

app/qtapp/rtkplot_qt/plotdata.cpp
@@ -51,7 +51,7 @@ void Plot::ReadSol(const QStringList &files, int sel)
     ShowMsg(tr("reading %1...").arg(paths[0]));
     ShowLegend(NULL);
 
-    if (!readsolt(paths, n, ts, te, tint, 0, &sol)) {
+    if (!readsolt((const char **)paths, n, ts, te, tint, 0, &sol)) {
         ShowMsg(tr("no solution data : %1...").arg(paths[0]));
         ShowLegend(NULL);
         ReadWaitEnd();
@@ -121,7 +121,7 @@ void Plot::ReadSolStat(const QStringList &files, int sel)
     ShowMsg(tr("reading %1...").arg(paths[0]));
     ShowLegend(NULL);
 
-    readsolstatt(paths, n, ts, te, tint, SolStat + sel);
+    readsolstatt((const char **)paths, n, ts, te, tint, SolStat + sel);
 
     UpdateSatList();
 }

lhuisman/myRTKLIB

app/qtapp/rtkplot_qt/plotdata.cpp
@@ -69,7 +69,7 @@ void Plot::readSolution(const QStringList &files, int sel)
     showMessage(tr("Reading %1...").arg(files.first()));
     showLegend(QStringList());
 
-    if (!readsolt(paths, n, ts, te, tint, SOLQ_NONE, &sol)) {
+    if (!readsolt((const char **)paths, n, ts, te, tint, SOLQ_NONE, &sol)) {
         showMessage(tr("No solution data: %1...").arg(files.first()));
         showLegend(QStringList());
         readWaitEnd();
@@ -145,7 +145,7 @@ void Plot::readSolutionStat(const QStringList &files, int sel)
     showMessage(tr("Reading %1...").arg(files.first()));
     showLegend(QStringList());
 
-    readsolstatt(paths, n, ts, te, tint, solutionStat + sel);
+    readsolstatt((const char **)paths, n, ts, te, tint, solutionStat + sel);
 
     updateSatelliteList();
 }