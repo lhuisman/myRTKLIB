8f554eb8f15f92dc29957b283407e5149a3d7c79
Jens Reimann <reimann_jens@arcor.de>
2024-06-28 11:36:03 +0200

    Qt Gui
    - fix timezone in timespan dialog
    - make time interval input a spin box

--- app/qtapp/appcmn_qt/tspandlg.cpp
+++ app/qtapp/appcmn_qt/tspandlg.cpp
@@ -31,7 +31,6 @@ void SpanDialog::showEvent(QShowEvent *event)
 {
     if (event->spontaneous()) return;
 
-
     updateEnable();
 }
 //---------------------------------------------------------------------------
@@ -39,7 +38,7 @@ void SpanDialog::updateEnable()
 {
     dTTimeStart->setEnabled(cBTimeStartEnabled->isChecked() && timeValid[0]);
     dTTimeEnd->setEnabled(cBTimeEndEnabled->isChecked() && timeValid[1]);
-    cBTimeInterval->setEnabled(cBTimeIntervalEnabled->isChecked() && timeValid[2]);
+    sBTimeInterval->setEnabled(cBTimeIntervalEnabled->isChecked() && timeValid[2]);
     cBTimeStartEnabled->setEnabled(timeValid[0] == 1);
     cBTimeEndEnabled->setEnabled(timeValid[1] == 1);
     cBTimeIntervalEnabled->setEnabled(timeValid[2] == 1);
@@ -50,8 +49,8 @@ void SpanDialog::showStartTimeDialog()
     TimeDialog * timeDialog = new TimeDialog(this);
 
     gtime_t time;
-    time.time = dTTimeStart->dateTime().toSecsSinceEpoch();
-    time.sec = dTTimeStart->dateTime().time().msec() / 1000;
+    time.time = dTTimeStart->dateTime().toUTC().toSecsSinceEpoch();
+    time.sec = dTTimeStart->dateTime().toUTC().time().msec() / 1000;
     timeDialog->setTime(time);
 
     timeDialog->exec();
@@ -73,44 +72,49 @@ gtime_t SpanDialog::getStartTime()
 {
     QDateTime start(dTTimeStart->dateTime());
     gtime_t timeStart;
-    timeStart.time = start.toSecsSinceEpoch(); timeStart.sec = start.time().msec() / 1000;
+
+    timeStart.time = start.toSecsSinceEpoch();
+    timeStart.sec = start.time().msec() / 1000;
+
     return timeStart;
 }
 //---------------------------------------------------------------------------
 void SpanDialog::setStartTime(gtime_t timeStart)
 {
-    QDateTime start = QDateTime::fromSecsSinceEpoch(timeStart.time);
+    QDateTime start = QDateTime::fromSecsSinceEpoch(timeStart.time, QTimeZone::UTC);
+
     start = start.addMSecs(timeStart.sec * 1000);
-    dTTimeStart->setTime(start.time());
-    dTTimeStart->setDate(start.date());
+
+    dTTimeStart->setDateTime(start);
 }
 //---------------------------------------------------------------------------
 gtime_t SpanDialog::getEndTime()
 {
     QDateTime end(dTTimeEnd->dateTime());
     gtime_t timeEnd;
-    timeEnd.time = end.toSecsSinceEpoch(); timeEnd.sec = end.time().msec() / 1000;
+
+    timeEnd.time = end.toSecsSinceEpoch();
+    timeEnd.sec = end.time().msec() / 1000;
+
     return timeEnd;
 }
 //---------------------------------------------------------------------------
 void SpanDialog::setEndTime(gtime_t timeEnd)
 {
-    QDateTime end = QDateTime::fromSecsSinceEpoch(timeEnd.time);
+    QDateTime end = QDateTime::fromSecsSinceEpoch(timeEnd.time, QTimeZone::UTC);
     end = end.addMSecs(timeEnd.sec * 1000);
 
-    dTTimeEnd->setTime(end.time());
-    dTTimeEnd->setDate(end.date());
-
+    dTTimeEnd->setDateTime(end);
 }
 //---------------------------------------------------------------------------
 double SpanDialog::getTimeInterval()
 {
-    return cBTimeInterval->currentText().toDouble();
+    return sBTimeInterval->value();
 }
 //---------------------------------------------------------------------------
 void SpanDialog::setTimeInterval(double timeInterval)
 {
-    cBTimeInterval->setCurrentText(QString::number(timeInterval));
+    sBTimeInterval->setValue(timeInterval);
 }
 //---------------------------------------------------------------------------
 int SpanDialog::getTimeEnable(int i)
@@ -142,6 +146,7 @@ int SpanDialog::getTimeValid(int i)
 void SpanDialog::setTimeValid(int i, int valid)
 {
     timeValid[i] = valid;
+
     updateEnable();
 }
 //---------------------------------------------------------------------------
