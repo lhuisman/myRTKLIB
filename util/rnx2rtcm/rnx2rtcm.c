ourairquality/RTKLIB

time_str: remove this non-reentrant function, replace with time2str
time2str() now returns a pointer to the string buffer which makes
it an easier replacement for the non-reentrant function time_str().

In particular time_str() was often used in trace function arguments
and if is useful to be able to avoid generating the time with not
emitting the trace output to keep these as trace function arguments.

util/rnx2rtcm/rnx2rtcm.c
@@ -166,8 +166,9 @@ static int conv_rtcm(const int *type, int n, const char *outfile,
         
         /* generate rtcm nav data messages */
         gen_rtcm_nav(rtcm.time,&rtcm,nav,index,type,n,fp);
-        
-        fprintf(stderr,"%s: NOBS=%2d\r",time_str(rtcm.time,0),rtcm.obs.n);
+
+        char tstr[40];
+        fprintf(stderr,"%s: NOBS=%2d\r",time2str(rtcm.time,tsrt,0),rtcm.obs.n);
     }
     /* generate rtcm nav data messages */
     gen_rtcm_nav(time0,&rtcm,nav,index,type,n,fp);

lhuisman/myRTKLIB

util/rnx2rtcm/rnx2rtcm.c
@@ -231,7 +231,8 @@ static int conv_rtcm(const int *type, int n, const char *opt, const char *outfil
     // Generate RTCM nav data messages
     gen_rtcm_nav(rtcm.time, &rtcm, nav, index, type, n, fp);
 
-    fprintf(stderr, "%s: NOBS=%2d\r", time_str(rtcm.time, 0), rtcm.obs.n);
+    char tstr[40];
+    fprintf(stderr, "%s: NOBS=%2d\r", time2str(rtcm.time, tstr, 0), rtcm.obs.n);
   }
   // Generate RTCM nav data messages
   gtime_t time0 = {0};